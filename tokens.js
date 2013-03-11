ac0b54a41f6b2583048e02fe61bd5e64c1629f55
Dioniz <frandioniz@gmail.com>
2013-03-11 11:04:22 +0000

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -26,80 +26,83 @@ String.prototype.tokens = function () {
     var m;                      // Matching
     var result = [];            // An array to hold the results.
 
-    var WHITES              = _______________________________________;
-    var ID                  = _______________________________________;
-    var NUM                 = _______________________________________;
-    var STRING              = _______________________________________;
-    var ONELINECOMMENT      = _______________________________________;
-    var MULTIPLELINECOMMENT = _______________________________________;
-    var TWOCHAROPERATORS    = _______________________________________;
-    var ONECHAROPERATORS    = _______________________________________;
+    var WHITES              = /\s+/g;
+    var ID                  = /[a-zA-Z_]\w*/g;
+    var NUM                 = /\d+(\.\d*)?([eE][+-]?\d+)?\b/g;
+    var STRING              = /('(\\.|[^'])*'|"(\\.|[^"])*")/g;
+    var ONELINECOMMENT      = /\/\/.*/g;
+    var MULTIPLELINECOMMENT = /\/[*](.|\n)*?[*]\//g;
+    var TWOCHAROPERATORS    = /([+][+=]|-[-=]|=[=<>]|[<>][=<>]|&&|[|][|])/g;
+    var ONECHAROPERATORS    = /([-+*\/=()&|;:<>[\]])/g;
+
 
     // Make a token object.
     var make = function (type, value) {
         return {
-            type: _____,
-            value: ______,
-            from: ______,
-            to: ____
+            type: type,
+            value: value,
+            from: from,
+            to: i
         };
     };
 
     // Begin tokenization. If the source string is empty, return nothing.
-    if (!this) return; 
+    if (!this) return;
+
+    var cadena = this; 
 
     // Loop through this text
-    while (_______________) {
-        WHITES.lastIndex =  ID.lastIndex = NUM.lastIndex = ______.lastIndex =
-        ONELINECOMMENT.lastIndex = ___________________.lastIndex =
-        ________________.lastIndex = ________________.lastIndex = _;
+     while (i < cadena.length) {
+        WHITES.lastIndex =  ID.lastIndex = NUM.lastIndex = STRING.lastIndex =
+        ONELINECOMMENT.lastIndex = MULTIPLELINECOMMENT.lastIndex =
+        TWOCHAROPERATORS.lastIndex = ONECHAROPERATORS.lastIndex = i;
         from = i;
         // Ignore whitespace.
-        if (m = WHITES.bexec(this)) {
+        if (m = WHITES.bexec(cadena)) {
             str = m[0];
-            _______________
+            cadena = cadena.substr(m.index + m[0].length);
+            
         // name.
-        } else if (m = ID.bexec(this)) {
+        } else if (m = ID.bexec(cadena)) {
             str = m[0];
-            _______________
+            cadena = cadena.substr(m.index + m[0].length);
             result.push(make('name', str));
 
         // number.
-        } else if (m = NUM.bexec(this)) {
+        } else if (m = NUM.bexec(cadena)) {
             str = m[0];
-            _______________
+            cadena = cadena.substr(m.index + m[0].length);
 
             n = +str;
             if (isFinite(n)) {
-                result.____(make('number', n));
+                result.push(make('number', n));
             } else {
                 make('number', str).error("Bad number");
             }
         // string
-        } else if (m = STRING.bexec(this)) {
+        } else if (m = STRING.bexec(cadena)) {
             str = m[0];
-            _______________
-            str = str.replace(/^____/,'');
+            cadena = cadena.substr(m.index + m[0].length);
+            str = str.replace(/^["']/,'');
             str = str.replace(/["']$/,'');
-            result.____(make('string', str));
+            result.push(make('string', str));
         // comment.
-        } else if ((m = ONELINECOMMENT.bexec(this))  || 
-                   (m = MULTIPLELINECOMMENT.bexec(this))) {
+        } else if ((m = ONELINECOMMENT.bexec(cadena))  || 
+                   (m = MULTIPLELINECOMMENT.bexec(cadena))) {
             str = m[0];
-            _______________
+            cadena = cadena.substr(m.index + m[0].length);
         // two char operator
-        } else if (m = TWOCHAROPERATORS.bexec(this)) {
+        } else if (m = TWOCHAROPERATORS.bexec(cadena)) {
             str = m[0];
-            _______________
-            result.____(make('operator', str));
+            cadena = cadena.substr(m.index + m[0].length);
+            result.push(make('operator', str));
         // single-character operator
-        } else if (m = ONECHAROPERATORS.bexec(this)){
-            result.____(make('operator', this.substr(i,1)));
-            _______________
+        } else if (m = ONECHAROPERATORS.bexec(cadena)){
+            result.push(make('operator', cadena.substr(i,1)));
+            cadena = cadena.substr(m.index + m[0].length);
         } else {
-          throw "Syntax error near '"+this.substr(i)+"'";
+          throw "Syntax error near '"+cadena.substr(i)+"'";
         }
     }
     return result;
 };
-
